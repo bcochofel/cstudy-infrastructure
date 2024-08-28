041e2a1db8902a7df89c1d3dfa435680472bf24c
Bruno Cochofel <bruno.cochofel@gmail.com>
2024-08-28 16:17:03 +0100

    feat: first commit

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# Automate Terraform with GitHub Actions
+# Case-Study Infrastructure
 
-This repo is a companion repo to the [Automate Terraform with GitHub Actions tutorial](https://developer.hashicorp.com/terraform/tutorials/automation/github-actions).
+This repository has Terraform code to deploy a case study infrastructure.
+
+## Terraform Modules
+
+The code from this repository uses some modules from both Azure and my own GitHub.
+
+Modules from my own repos have some static code analysis tools, used with pre-commit, and also use semantic versioning and conventional commits.
+Here's the list of modules:
+
+- [terraform-azurerm-resource-group](https://github.com/bcochofel/terraform-azurerm-resource-group)
+- [terraform-azurerm-virtual-network](https://github.com/bcochofel/terraform-azurerm-virtual-network)
+- [terraform-azurerm-subnet](https://github.com/bcochofel/terraform-azurerm-subnet)
+
+All the modules have the same GitHub Workflows, and use the following tools:
+
+- [pre-commit](https://pre-commit.com/)
+- [semantic-release](https://semantic-release.gitbook.io/semantic-release)
+- [terratest](https://terratest.gruntwork.io/)
+
+They are available in Terraform Registry, and use Terraform naming convention for modules, alongside examples and automatically generated documentation.
+
+List of static code analysis tools:
+
+- [TFlint w/ AzureRM plugin](https://github.com/terraform-linters/tflint)
+- [TFsec](https://github.com/aquasecurity/tfsec)
+- [checkov](https://www.checkov.io/)
+- [terraform-docs](https://terraform-docs.io/)
+
+The semantic-release ensures that a new release is created every time a merge to the default branch is triggered, and also generates a CHANGELOG.md file from the conventional commit messages.
--- main.tf
+++ main.tf
@@ -1,74 +1,29 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
+provider "azurerm" {
+  features {}
 }
 
-provider "aws" {
-  region = "us-west-2"
+module "naming" {
+  #checkov:skip=CKV_TF_1:Ensure Terraform module sources use a commit hash
+  source  = "Azure/naming/azurerm"
+  version = "0.4.1"
+
+  suffix = [
+    var.workload,
+    var.environment,
+    var.location
+  ]
 }
 
-resource "random_pet" "sg" {}
+module "rg" {
+  #checkov:skip=CKV_TF_1:Ensure Terraform module sources use a commit hash
+  source  = "bcochofel/resource-group/azurerm"
+  version = "1.6.0"
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+  name     = module.naming.resource_group.name_unique
+  location = var.location
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  tags = {
+    ManagedBy   = "Terraform"
+    Environment = var.environment
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
 }
