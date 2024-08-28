1551802f0a4f874039f7d5099bff85c9e26ab7d5
Bruno Cochofel <bruno.cochofel@gmail.com>
2024-08-28 16:59:47 +0100

    fix: Resource Group tags

--- main.tf
+++ main.tf
@@ -21,29 +21,30 @@ module "rg" {
   tags = var.tags
 }
 
-module "vnet" {
-  #checkov:skip=CKV_TF_1:Ensure Terraform module sources use a commit hash
-  source  = "bcochofel/virtual-network/azurerm"
-  version = "1.3.0"
-
-  resource_group_name = module.rg.name
-  name                = module.naming.virtual_network.name_unique
-  address_space       = ["10.0.0.0/16"]
-
-  tags = var.tags
-}
-
-module "snet" {
-  #checkov:skip=CKV_TF_1:Ensure Terraform module sources use a commit hash
-  source  = "bcochofel/subnet/azurerm"
-  version = "1.4.0"
-
-  name                    = module.naming.subnet.name_unique
-  resource_group_name     = module.rg.name
-  resource_group_location = module.rg.location
-  nsg_name                = module.naming.network_security_group.name_unique
-  virtual_network_name    = module.vnet.name
-  address_prefixes        = ["10.0.0.0/24"]
-
-  tags = var.tags
-}
+#module "vnet" {
+#  #checkov:skip=CKV_TF_1:Ensure Terraform module sources use a commit hash
+#  source  = "bcochofel/virtual-network/azurerm"
+#  version = "1.3.0"
+#
+#  resource_group_name = module.rg.name
+#  name                = module.naming.virtual_network.name_unique
+#  address_space       = ["10.0.0.0/16"]
+#
+#  tags = var.tags
+#}
+#
+#module "snet" {
+#  #checkov:skip=CKV_TF_1:Ensure Terraform module sources use a commit hash
+#  source  = "bcochofel/subnet/azurerm"
+#  version = "1.4.0"
+#
+#  name                    = module.naming.subnet.name_unique
+#  resource_group_name     = module.rg.name
+#  resource_group_location = module.rg.location
+#  nsg_name                = module.naming.network_security_group.name_unique
+#  virtual_network_name    = module.vnet.name
+#  address_prefixes        = ["10.0.0.0/24"]
+#
+#  tags = var.tags
+#}
+#
